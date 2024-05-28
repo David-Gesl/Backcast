2601bc50550d14858242d1c16d6e91e8ea2c4f1c
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2024-05-28 01:27:59 -0700

    Added messages for generation

--- src/show.py
+++ src/show.py
@@ -4,15 +4,20 @@ from ytmusic import getMusic
 from retrofy import getShow
 from random import randint
 
+print("Getting script...")
 talk = getScript()
 
+print("Getting vocals...")
 vocals = getVocal(talk)
 
 # will fix this later
+print("Getting music...")
 # music = getMusic()
 
 # get a random music files from the templates folder
 music = f"./templates/music{randint(1, 3)}.mp3"
 
+print("Generating show...")
 show = getShow(vocals, music)
+print("Finished generating show.")
 
