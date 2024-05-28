0ba90dcf4a4a7824bf6e0bd8ac7a35ad688b71d7
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2024-05-28 13:30:32 -0700

    Added download ranges

--- src/music.py
+++ src/music.py
@@ -8,10 +8,19 @@ URLS = ["https://youtube.com/playlist?list=PLz1WANnSJES9Yesv3Wk7kSjD7VTcGdLxw&si
 
 random.seed(int(datetime.datetime.now().timestamp()))
 
+def download_ranges(info_dict, ydl):
+    # Example video sections to download
+    sections = [
+        {"start_time": 0, "end_time": 900},
+    ]
+
+    return sections
+
 ydl_opts = {
-    'paths': {'home':'./retromusic'},
+    'paths': {'home':'./templates'},
     'playlistrandom':'True',
     'max_downloads':'1',
+    'download_ranges': download_ranges,
     'format': 'mp3/bestaudio/best',
     'postprocessors': [{  # Extract audio using ffmpeg
         'key': 'FFmpegExtractAudio',
